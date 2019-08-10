c542a6510979d263c6db876c7f480f3218e3fec3
kautsaralvin <alvinrk8@gmail.com>
2019-08-10 21:52:48 +0700

    menambahkan nama alvin

--- program.py
+++ program.py
@@ -1,9 +1,11 @@
 ### KODINGAN UTAMA
 
-nama = "Wisnu";
-print("Hallo "+nama);
+nama = "Wisnu"
+print("Hallo "+nama)
 
 ### BAGIANNYA ALVIN
+nama = "Alvin"
+print("Hallo "+nama)
 
 ### BAGIANNYA IZZUR
 
